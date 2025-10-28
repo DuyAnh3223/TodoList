f3905f70776bd99951569db33f1ab54662e819c8
Duy Anh <duyanh3203@gmail.com>
2025-10-28 23:04:21 +0700

    feat: add Header.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,34 @@
+import AddTask from "@/components/AddTask";
+import DateTimeFilter from "@/components/DateTimeFilter";
+import Footer from '@/components/Footer'
+import Header from "@/components/Header";
+import StatsAndFilters from '@/components/StatsAndFilters'
+import TaskList from '@/components/TaskList'
+import TaskListPagination from '@/components/TaskListPagination'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!!!</h1>
+    <div className="container pt-8 mx-auto">
+        <div className="w-full max-w-2xl p-6 mx-auto space-y-6">
+            {/* Đầu trang*/}
+            <Header/>
+            {/* Tạo nhiệm vụ */}
+            <AddTask/>
+            {/* Thống kê và bộ lọc */}
+            <StatsAndFilters/>
+            {/* Danh sách nhiệm vụ */}
+            <TaskList/>
+            {/* Phân trang danh sách nhiệm vụ và Lọc theo Date */}
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+                <TaskListPagination/>
+                <DateTimeFilter/>
+            </div>
+            {/*Chân trang */}
+            <Footer/>
+
+            
+        </div>
     </div>
   )
 }
